278fc9cfc4cc17849cd9d981013c09cff9d429ea
Sraboni <srabonidatta9@gmail.com>
2023-04-23 04:04:31 -0500

    register

---
+++ src/Form.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react'
+import './style.css'
+function Form() {
+    const data = {name:"", email:"", password:""};
+    const [inputData, setInputData] = useState(data)
+    const [flag, setFlag] = useState(false)
+    useEffect(()=>{
+        console.log("Registered")
+    },[flag])
+    function handleData(e){
+        setInputData({...inputData, [e.target.name]:e.target.value})
+        console.log(inputData)
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        if(!inputData.name || !inputData.email || !inputData.password){
+            alert("All fields are Mendatory")
+        }
+        else{
+            setFlag(true)
+        }
+    }
+  return (
+    <>
+    <pre>{(flag)?<p className='ui-define'>Hello {inputData.name}, You've Registered Successfully!!</p>:""}</pre>
+    <form className='container' onSubmit={handleSubmit}>
+        <div className='header'>
+            <h1>Create Your Account</h1>
+        </div>
+        <div>
+            <input type="text" placeholder='Enter Your Name' name='name' value={inputData.name} onChange={handleData}/>
+        </div>
+        <div>
+            <input type="text" placeholder='Enter Your Email' name='email' value={inputData.email} onChange={handleData}/>
+        </div>
+        <div>
+            <input type="text" placeholder='Enter Your Password' name='password' value={inputData.password} onChange={handleData}/>
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+
+    </>
+  )
+}
+
+export default Form
